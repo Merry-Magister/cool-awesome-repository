6eb2018c24467134bd363eaa739410a2b08f646c
Magister <hep.bragaiscailiavrencelv@gmail.com>
2026-09-04 11:20:37 +0800

    Create q1_sg5_a1_samat_bragais.py

---
+++ q1_sg5_a1_samat_bragais.py
@@ -0,0 +1,11 @@
+class Hero:
+  def __init__(self, name, hp):
+    self.name = name
+    self.hp = hp
+    
+  def take_damage(hp, amount):
+    self.hp -= self.amount
+    
+  def hp_tracker(name, hp):
+    print(f"{self.name} has {hp}
+    
