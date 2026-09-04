873c498b30310f49dd3323d1ea3165f3216005d2
Magister <hep.bragaiscailiavrencelv@gmail.com>
2026-09-04 12:51:29 +0800

    Update q1_sg5_a1_samat_bragais.py

--- q1_sg5_a1_samat_bragais.py
+++ q1_sg5_a1_samat_bragais.py
@@ -1,11 +1,19 @@
 class Hero:
-  def __init__(self, name, hp):
+  def __init__(self, name, hp=25):
     self.name = name
     self.hp = hp
     
-  def take_damage(hp, amount):
-    self.hp -= self.amount
+  def take_damage(self, amount):
+    self.hp -= amount
+    print(f"{self.name} took {amount} damage!")
     
-  def hp_tracker(name, hp):
-    print(f"{self.name} has {hp}
-    
+  def hp_tracker(self):
+    print(f"{self.name} currently has {self.hp} hp.")
+
+arthur = Hero("Arthur")
+morgana = Hero("Morgana")
+
+arthur.take_damage(10)
+
+arthur.hp_tracker()
+morgana.hp_tracker()
