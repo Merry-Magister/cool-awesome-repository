1a9e2022ef51f2d1f9af494ebbfd4803e32ef657
Magister <hep.bragaiscailiavrencelv@gmail.com>
2026-09-11 19:00:21 +0800

    Did some debugging to sg7’s answer

--- q1_sg7_samat_bragais.py
+++ q1_sg7_samat_bragais.py
@@ -1,28 +1,36 @@
 class Glassware:
     def __init__(self, name):
         self.name = name
-        print(f"a {self.name} will be used")
+        print(f"\na {self.name} will be used")
+        
+    def putAway(self, name):
+        print(f"putting away the {self.name}...")
 
-class Beaker:
+class Beaker(Glassware):
     def __init__(self, name):
-       super().__init___(name)
-       print(f"\nI have a {self.name}")
+       super().__init__(name)
+       print(f"I have a {self.name}")
+    
     def __del__(self):
-        ("Beaker deleted !!")
+        (f"a {self.name} was deleted !!")
+        
+    def putAway(self, name):
+        super().putAway(self, name)
 
 class Tray:
     def __init__(self, capacity = 5):
         self.capacity = capacity
         print("\nI have a tray")
-        self.beaker = Beaker
+        self.firstBeaker = Beaker("beaker filled with water")
+        self.secondBeaker = Beaker("small beaker filled with some oil")
+        self.thirdBeaker = Beaker("currently empty beaker")
+        self.fourthBeaker = Beaker("recently cleaned beaker")
+        self.fifthBeaker = Beaker("beaker filled with saltwater")
+    
     def __del__(self):
         ("Tray deleted !!")
-        del self.beaker
+        del self.firstBeaker; del self.secondBeaker; del self.thirdBeaker; del self.fourthBeaker; del self.fifthBeaker
 
 tray = Tray()
-waterBeaker = Beaker("beaker filled with water")
-oilBeaker = Beaker("small beaker filled with some oil")
-emptyBeaker = Beaker("currently empty beaker")
-newBeaker = Beaker("recently cleaned beaker")
-saltBeaker = Beaker("beaker filled with saltwater")
+tray.firstBeaker.putAway
 del tray
